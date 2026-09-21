3d4657b2a74990bc93abca7654a5d7129a233297
Saurabh Sharma <srb.sharma8@gmail.com>
2026-09-21 15:58:39 +0530

    Add SHUNYA terminal session experience

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,28 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Suppress Fish's default welcome message.
+    set -g fish_greeting ""
+
+    # Show SHUNYA system information only in the first Kitty
+    # shell of the current login session.
+    if set -q KITTY_WINDOW_ID
+        set -l marker "$XDG_RUNTIME_DIR/shunya-fastfetch-shown"
+
+        if not test -e "$marker"
+            touch "$marker"
+
+            if type -q fastfetch
+                fastfetch
+            end
+        end
+    end
+
+    # SHUNYA prompt
+    if type -q starship
+        function starship_transient_prompt_func
+            starship module character
+        end
+
+        starship init fish | source
+        enable_transience
+    end
 end
